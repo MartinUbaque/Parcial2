bd4ea11aab8ff9ac28cab25441405be5a6067ef5
martinubaque <m.ubaque@uniandes.edu.co>
2023-11-30 10:44:46 -0500

    Pruebas unitarias

--- src/album/album.service.spec.ts
+++ src/album/album.service.spec.ts
@@ -1,18 +1,57 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { Repository } from 'typeorm';
+import { faker } from '@faker-js/faker';
+import { AlbumFotoService } from 'src/album-foto/album-foto.service';
 import { AlbumService } from './album.service';
+import { AlbumEntity } from './album.entity/album.entity';
 
-describe('AlbumService', () => {
+describe('FotoService', () => {
   let service: AlbumService;
+  let repository: Repository<AlbumEntity>;
+  let albumList: AlbumEntity[];
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
       providers: [AlbumService],
     }).compile();
 
     service = module.get<AlbumService>(AlbumService);
+    repository = module.get<Repository<AlbumEntity>>(
+      getRepositoryToken(AlbumEntity),
+    );
+    await seedDatabase();
   });
+
+  const seedDatabase = async () => {
+    repository.clear();
+    for (let i = 0; i < 5; i++) {
+      const album: AlbumEntity = await repository.save({
+
+        titulo: faker.lorem.word(5),
+        fechaInicio:faker.date.past(),
+        fechaFin:faker.date.past()
+
+      });
+      albumList.push(album);
+    }
+  };
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+
+  it('deleteAlbum should remove a album', async () => {
+    const strAlbum: AlbumEntity = albumList[0];
+    await service.deleteAlbum(strFoto.id);
+    const deletedFoto: FotoEntity = await repository.findOne({
+      where: { id: strFoto.id }, 
+    });
+
+    expect(deletedFoto).toBeNull(); 
   });
 });
--- src/foto/foto.service.spec.ts
+++ src/foto/foto.service.spec.ts
@@ -1,18 +1,79 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
+import { Repository } from 'typeorm';
+import { faker } from '@faker-js/faker';
 import { FotoService } from './foto.service';
+import { FotoEntity } from './foto.entity/foto.entity';
 
 describe('FotoService', () => {
   let service: FotoService;
+  let repository: Repository<FotoEntity>;
+  let fotoList: FotoEntity[];
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [...TypeOrmTestingConfig()],
       providers: [FotoService],
     }).compile();
 
     service = module.get<FotoService>(FotoService);
+    repository = module.get<Repository<FotoEntity>>(
+      getRepositoryToken(FotoEntity),
+    );
+    await seedDatabase();
   });
+
+  const seedDatabase = async () => {
+    repository.clear();
+    for (let i = 0; i < 5; i++) {
+      const foto: FotoEntity = await repository.save({
+        ISO: faker.number.int({min:100,max:6400}),
+        valObturacion: faker.number.int({min:2,max:250}),
+        apertura: faker.number.int({min:1,max:32}),
+        fecha:faker.date.past()
+
+      });
+      fotoList.push(foto);
+    }
+  };
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('createFoto should create a new foto', async () => {
+    const foto: FotoEntity = await repository.save({
+      ISO: faker.number.int({min:100,max:6400}),
+      valObturacion: faker.number.int({min:2,max:250}),
+      apertura: faker.number.int({min:1,max:32}),
+      fecha:faker.date.past()
+
+    });
+    const newFoto: FotoEntity = await service.create(foto);
+    expect(newFoto).not.toBeNull();
+
+    const strFoto: FotoEntity = await repository.findOne({
+      where: { id: newFoto.id },
+    });
+    expect(strFoto).not.toBeNull();
+    expect(strFoto.ISO).toEqual(strFoto.ISO);
+    expect(strFoto.velObturacion).toEqual(strFoto.velObturacion);
+  });
+
+  it('deleteFoto should remove a foto', async () => {
+    const strFoto: FotoEntity = fotoList[0];
+    await service.delete(strFoto.id);
+    const deletedFoto: FotoEntity = await repository.findOne({
+      where: { id: strFoto.id }, 
+    });
+
+    expect(deletedFoto).toBeNull(); 
+  });
+
+
+
 });
+
+
