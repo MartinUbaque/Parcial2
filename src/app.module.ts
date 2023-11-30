f08940fb97f4f3cc46ddce1233d9e82af8e44537
martinubaque <m.ubaque@uniandes.edu.co>
2023-11-30 09:39:55 -0500

    foto module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EntidadModule } from './entidad/entidad.module';
-import { MuseoModule } from './museo/museo.module';
-import { DueñoModule } from './dueño/dueño.module';
+
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { EntidadEntity } from './entidad/entidad.entity/entidad.entity'; 
-import { MuseoEntity } from './museo/museo.entity/museo.entity';
-import { DueñoEntity } from './dueño/dueño.entity/dueño.entity';
+
+import { FotoModule } from './foto/foto.module';
+import { FotoEntity } from './foto/foto.entity/foto.entity';
 
 @Module({
-  imports: [EntidadModule, MuseoModule, DueñoModule,
+  imports: [FotoModule,
     TypeOrmModule.forRoot({
     type: 'postgres',
     host: 'localhost',
@@ -18,7 +16,7 @@ import { DueñoEntity } from './dueño/dueño.entity/dueño.entity';
     username: 'postgres',
     password: 'postgres',
     database: 'museum',
-    entities: [EntidadEntity, MuseoEntity, DueñoEntity], 
+    entities: [FotoEntity], 
     dropSchema: true,
     synchronize: true,
     keepConnectionAlive: true
